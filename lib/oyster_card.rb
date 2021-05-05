7a2270ae9fcb634911551b9c758bf02f40c498d2
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-05 14:32:11 +0100

    completed challenge 8

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -8,6 +8,7 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -18,5 +19,17 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+  
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -25,4 +25,27 @@ describe Oystercard do
       expect { subject.deduct 1 }.to change { subject.balance }.by(-1)
     end
   end
+
+  describe '#in_journey?' do
+    context 'before journey' do
+      it 'returns false' do
+        expect(subject).not_to be_in_journey
+      end
+    end
+
+    context 'during journey' do
+      it 'returns true' do
+        subject.touch_in
+        expect(subject).to be_in_journey
+      end
+    end
+    
+    context 'after journey' do
+      it 'returns false' do
+        subject.touch_in
+        subject.touch_out
+        expect(subject).not_to be_in_journey
+      end
+    end
+  end
 end
