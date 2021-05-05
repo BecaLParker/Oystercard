0077893b5176d90b93e1fad510df56fc827661f9
BecaLParker <becaparker@gmail.com>
2021-05-05 16:10:10 +0100

    step 11 (saving entry station)

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -2,14 +2,13 @@
 
 # seems like rubocop wants a comment here?
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
   MAXIMUM_LIMIT = 90
   MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
-    @in_journey = false
   end
 
   def top_up(amount)
@@ -19,20 +18,20 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    !@entry_station.nil?
   end
 
-  def touch_in
+  def touch_in(station)
     raise 'Balance is below minimum required for journey' if @balance < MINIMUM_FARE
 
-    @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
     deduct(MINIMUM_FARE)
-    @in_journey = false
+    @entry_station = nil
   end
-  
+
   private
 
   def deduct(amount)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,10 @@
 require 'oyster_card'
 
 describe Oystercard do
+  let(:station) { double('station') }
+  # let(:card_in_journey) { subject.touch_in(station)}
+  # allows?
+
   describe '#balance' do
     it 'a freshly initialised card has a balance of 0' do
       expect(subject.balance).to eq 0
@@ -33,14 +37,14 @@ describe Oystercard do
 
     context 'during journey' do
       it 'returns true' do
-        subject.touch_in
+        subject.touch_in(station)
         expect(subject).to be_in_journey
       end
     end
 
     context 'after journey' do
       it 'returns false' do
-        subject.touch_in
+        subject.touch_in(station)
         subject.touch_out
         expect(subject).not_to be_in_journey
       end
@@ -50,14 +54,35 @@ describe Oystercard do
   describe '#touch_in' do
     context 'balance is below minimum' do
       it 'raises an excpetion' do
-        expect { subject.touch_in }.to raise_error 'Balance is below minimum required for journey'
+        expect { subject.touch_in(station) }.to raise_error 'Balance is below minimum required for journey'
+      end
+    end
+
+    context 'with a sufficient balance' do
+      before do
+        subject.top_up(10)
+      end
+
+      it 'set the entry station' do
+        subject.touch_in(station)
+        expect(subject.entry_station).to eq(station)
       end
     end
   end
 
   describe '#touch_out' do
+    before do
+      subject.top_up(10)
+      subject.touch_in(station)
+    end
+
     it 'reduces balance by the minimum fare' do
-      expect { subject.touch_out }.to change{ subject.balance }.by(-1)
+      expect { subject.touch_out }.to change { subject.balance }.by(-1)
+    end
+
+    it 'sets entry_station to nil' do
+      subject.touch_out
+      expect(subject.entry_station).to eq nil
     end
   end
 end
